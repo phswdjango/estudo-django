b991fd5f7085475fcf2e702de8e03648fbc99018
phsw <pedromadureira000@gmail.com>
2021-03-03 20:47:52 -0300

    SECRET_KEY desacoplada

--- modelodjango/settings.py
+++ modelodjango/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ls$$@r1-a+vs@afekd)t*=3@)php^cl7d*e7z#ffnz35v4*r!c'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
