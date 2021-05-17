82fe2394ba7e6d08da33707fa1c4c4a630937d78
phsw <pedromadureira000@gmail.com>
2021-05-17 10:59:36 -0300

    Aula: Monitorando Erros com Sentry

--- modelodjango/settings.py
+++ modelodjango/settings.py
@@ -17,6 +17,9 @@ from pathlib import Path
 from decouple import config, Csv
 import dj_database_url
 
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -207,3 +210,20 @@ if AWS_ACCESS_KEY_ID:  # pragma: no cover
     # começa com '//' por que vai seguir o protocolo no qual ele for inserido (https, http,)
     INSTALLED_APPS.append('s3_folder_storage')
     INSTALLED_APPS.append('storages')  # adicionar essas libs apenas se estiver com AWS configurado.
+
+SENTRY_DSN = config('SENTRY_DSN', default=None)
+
+if SENTRY_DSN:
+    sentry_sdk.init(
+        dsn=SENTRY_DSN,
+        integrations=[DjangoIntegration()],
+
+        # Set traces_sample_rate to 1.0 to capture 100%
+        # of transactions for performance monitoring.
+        # We recommend adjusting this value in production.
+        traces_sample_rate=1.0,
+
+        # If you wish to associate users to errors (assuming you are using
+        # django.contrib.auth) you may enable sending PII data.
+        send_default_pii=True
+    )
