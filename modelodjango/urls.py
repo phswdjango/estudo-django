d77411e181e7589321437f15b736321d3cb2e4f2
phsw <pedromadureira000@gmail.com>
2021-05-19 10:20:21 -0300

    Aula:Encapsulando Urls em Apps (mudando o url "/" para "". Por causa de um bug no deploy do heroku.)

--- modelodjango/urls.py
+++ modelodjango/urls.py
@@ -19,7 +19,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/', include('modelodjango.base.urls')),
+    path('', include('modelodjango.base.urls')),
 ]
 
 if settings.DEBUG:
